f0bf4e95ab6a51579b4967ee08358435afde4556
Abraham Bishop <crypticcodetechnologies@gmail.com>
2025-09-12 15:35:35 +0100

    Removed multer storage config

--- libs/multer.js
+++ libs/multer.js
@@ -3,9 +3,6 @@ const multer = require('multer')
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../", "uploads"))
-    },
     filename: (req, file, cb) => {
         const filename = `${file.fieldname}-${Date.now()}-${Math.floor(Math.random() * 1E9)}${path.extname(file.originalname)}`;
         cb(null, filename)
