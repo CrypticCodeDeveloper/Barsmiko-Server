ccf77336967b978d44d3fb73db2b3ca01ec8e48e
Abraham Bishop <crypticcodetechnologies@gmail.com>
2025-09-14 23:38:15 +0100

    function to filter blog posts by title using search query

--- controllers/blog.js
+++ controllers/blog.js
@@ -13,7 +13,12 @@ const uploadToCloudinary = async (path) => {
 }
 
 const getAllPosts = async (req, res) => {
-    const blogs = await Blog.find({}).populate("author").sort({ createdAt: -1 })
+
+    const {search} = req.query || "";
+
+    const filter = search ? {title: {$regex: search, $options: "i"}} : {}
+
+    const blogs = await Blog.find(filter).populate("author").sort({ createdAt: -1 })
     res.status(200).json({
         data: {
             message: "All blog posts",
